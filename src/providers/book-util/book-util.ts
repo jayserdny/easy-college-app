73b4649476f182a90107f6fd83b756233b8422ec
Jayser Mendez <jayser.mendez@stu.bcc.cuny.edu>
2017-11-11 01:23:21 -0500

    addBook function added

--- src/providers/book-util/book-util.ts
+++ src/providers/book-util/book-util.ts
@@ -1,18 +1,37 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { Book } from '../../models/models';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
 
-/*
-  Generated class for the BookUtilProvider provider.
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class BookUtilProvider {
 
-  constructor(public http: Http) {
-    console.log('Hello BookUtilProvider Provider');
+  // Initialize a collection for the books 
+  private bookCollection: AngularFirestoreCollection<Book>;
+
+  constructor(private afs: AngularFirestore) {
+    // Make a reference of the collection in firebase
+    this.bookCollection = afs.collection<Book>('books');
   }
+
+  /**
+   * Method to add a book to firestore in firebase
+   * @param book 
+   */
+  addBook(book: Book) {
+    return new Promise((resolve, reject) => {
+      this.bookCollection.add(book).then(data => {
+        resolve(data)
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  }
+
+  
+
+
 
 }
