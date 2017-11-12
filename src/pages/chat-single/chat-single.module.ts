08a0e465d8934071f35468aed051870215db33b1
Jewel Clarke-Henry <jewelclarkehenry@gmail.com>
2017-11-11 20:46:36 -0500

    change time

--- src/pages/chat-single/chat-single.module.ts
+++ src/pages/chat-single/chat-single.module.ts
@@ -2,12 +2,14 @@
 import { MessagesPage } from './chat-single';
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
+import { MomentModule } from 'angular2-moment';
 
 @NgModule({
   declarations: [
     MessagesPage,
   ],
   imports: [
+    MomentModule,
     IonicPageModule.forChild(MessagesPage),
   ],
   exports: [
